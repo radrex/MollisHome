aef18a760d3d51c1965a8a3c95c6dfa17384b11f
radrex <radostinv.stoychev@gmail.com>
2021-03-12 05:22:20 +0200

    Add Service Test Layer

---
+++ MollisHome/Tests/MollisHome.Services.Data.Tests/DbFixture.cs
@@ -0,0 +1,38 @@
+﻿namespace MollisHome.Services.Data.Tests
+{
+    using MollisHome.Data;
+    using MollisHome.Data.Models;
+
+    using MollisHome.Services.Data.Products;
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using System;
+
+    public class DbFixture
+    {
+        //------------- CONSTRUCTORS --------------
+        public DbFixture()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+            services.AddIdentityCore<ApplicationUser>(IdentityOptionsProvider.GetIdentityOptions)
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddAutoMapper(x =>
+            {
+                x.AddProfile(new AutoMapperDTOConfiguration());
+            });
+
+            services.AddTransient<IProductsService, ProductsService>();
+
+            this.ServiceProvider = services.BuildServiceProvider();
+        }
+
+        //-------------- PROPERTIES ---------------
+        public ServiceProvider ServiceProvider { get; private set; }
+    }
+}
---
+++ MollisHome/Tests/MollisHome.Services.Data.Tests/ProductsServiceTests.cs
@@ -0,0 +1,54 @@
+﻿namespace MollisHome.Services.Data.Tests
+{
+    using MollisHome.Data;
+    using MollisHome.Data.Seeding;
+    using MollisHome.Services.Data.Products;
+
+    using Xunit;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class ProductsServiceTests : IClassFixture<DbFixture>
+    {
+        //---------------- FIELDS -----------------
+        private ServiceProvider serviceProvider;
+
+        //------------- CONSTRUCTORS --------------
+        public ProductsServiceTests(DbFixture fixture)
+        {
+            this.serviceProvider = fixture.ServiceProvider;
+            ApplicationDbContext dbContext = this.serviceProvider.GetRequiredService<ApplicationDbContext>();
+            new DatabaseSeeder().SeedAsync(dbContext, this.serviceProvider).GetAwaiter().GetResult();
+            this.productsService = this.serviceProvider.GetRequiredService<IProductsService>();
+        }
+
+        //-------------- PROPERTIES ---------------
+        private IProductsService productsService { get; }
+
+        //--------------- METHODS -----------------
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(3, 100)]
+        public void GetNewestProducts_ReturnsCorrectNumberOfItems(int expected, int n)
+        {
+            Assert.Equal(expected, productsService.GetNewestProducts(n).Count());
+        }
+
+        [Theory]
+        [InlineData(new int[] { }, -1)]
+        [InlineData(new int[] { 3 }, 1)]
+        [InlineData(new int[] { 3, 2 }, 2)]
+        [InlineData(new int[] { 3, 2, 1 }, 3)]
+        [InlineData(new int[] { 3, 2, 1 }, 100)]
+        public void GetNewestProducts_ReturnsCorrectItems(int[] expectedIds, int n)
+        {
+            IEnumerable<int> productIds = productsService.GetNewestProducts(n).Select(x => x.Id);
+            Assert.True(expectedIds.SequenceEqual(productIds));
+        }
+    }
+}
